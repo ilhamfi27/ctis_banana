3ee6541493e4f2ab46f986a391ab9ae6b5747b44
faizazharr <43517978+faizazharr@users.noreply.github.com>
2019-01-23 11:51:37 +0700

    Create banana.sql

---
+++ db/banana.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Jan 2019 pada 05.51
+-- Versi server: 10.1.34-MariaDB
+-- Versi PHP: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banana`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `cuaca`
+--
+
+CREATE TABLE `cuaca` (
+  `id_sensor` varchar(50) NOT NULL,
+  `kelembapan` double NOT NULL,
+  `angin` double NOT NULL,
+  `suhu` double NOT NULL,
+  `curah_hujan` double NOT NULL,
+  `cuaca` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `laporan`
+--
+
+CREATE TABLE `laporan` (
+  `id_lokasi` varchar(50) NOT NULL,
+  `waktu_kejadian` time NOT NULL,
+  `deskripsi` text NOT NULL,
+  `kategori_kejadian` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lokasi`
+--
+
+CREATE TABLE `lokasi` (
+  `id_sensor` varchar(50) NOT NULL,
+  `nama` text NOT NULL,
+  `longitude` double NOT NULL,
+  `latitude` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `cuaca`
+--
+ALTER TABLE `cuaca`
+  ADD KEY `iddaerah` (`id_sensor`);
+
+--
+-- Indeks untuk tabel `laporan`
+--
+ALTER TABLE `laporan`
+  ADD KEY `iddaerah` (`id_lokasi`);
+
+--
+-- Indeks untuk tabel `lokasi`
+--
+ALTER TABLE `lokasi`
+  ADD PRIMARY KEY (`id_sensor`);
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `cuaca`
+--
+ALTER TABLE `cuaca`
+  ADD CONSTRAINT `cuaca_ibfk_1` FOREIGN KEY (`id_sensor`) REFERENCES `lokasi` (`id_sensor`);
+
+--
+-- Ketidakleluasaan untuk tabel `laporan`
+--
+ALTER TABLE `laporan`
+  ADD CONSTRAINT `laporan_ibfk_1` FOREIGN KEY (`id_lokasi`) REFERENCES `lokasi` (`id_sensor`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
