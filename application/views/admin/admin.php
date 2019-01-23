cd4728741933cbc95e0734cd9ded7aea493c89d8
faizazharr <43517978+faizazharr@users.noreply.github.com>
2019-01-23 14:49:02 +0700

    update admin

--- application/views/admin/admin.php
+++ application/views/admin/admin.php
@@ -75,9 +75,11 @@
             </div>
         </aside>
     </section>
-    <script src=<?php echo base_url('assets/jquery/jquery.min.js'); ?>></script>
-    <script src=<?php echo base_url('assets/venndor/common-scripts.js'); ?>></script>
-    <script class="include" type="text/javascript" src=<?php echo base_url('assets/vendor/jquery/jquery.dcjqaccordion.2.7.js'); ?>></script>
+    <footer>
+        <script src=<?php echo base_url('assets/jquery/jquery.min.js'); ?>></script>
+        <script src=<?php echo base_url('assets/venndor/common-scripts.js'); ?>></script>
+        <script class="include" type="text/javascript" src=<?php echo base_url('assets/vendor/jquery/jquery.dcjqaccordion.2.7.js'); ?>></script>
+    </footer>
 </body>
 
 </html>
---
+++ application/views/resources/sideradmin.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+    <aside>
+    <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu" id="nav-accordion">
+            <p class="centered">
+                <a href="profile.html"><img src="<?php echo base_url('assets/img/user.png'); ?>" class="img-circle" width="80"></a>
+            </p>
+            <h5 class="centered">Admin</h5>
+            <li class="mt">
+                <a href="admin.php">
+                    <i class="fa fa-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="sub-menu">
+                <a href="">
+                    <i class="fa fa-flag"></i>
+                    <span>Set Lokasi</span>
+                </a>
+
+            </li>
+            <li class="sub-menu">
+                <a href="">
+                    <i class="fa fa-book"></i>
+                    <span>Laporan</span>
+                </a>
+
+            </li>
+            <li class="sub-menu">
+                <a href="">
+                    <i class="fa fa-twitter"></i>
+                    <span>cek twitter</span>
+                </a>
+
+            </li>
+            <li>
+                <a href="google_maps.html">
+                    <i class="fa fa-map-marker"></i>
+                    <span>Maps </span>
+                </a>
+            </li>
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+    </aside>
+</section>
